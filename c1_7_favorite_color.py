14ac4bab4c4850e05c5a7b12419d515cab1e4853
highsociety2 <amcknight@ideaventionsacademy.org>
2017-11-21 11:54:45 -0500

    hi

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -1,5 +1,46 @@
-# Name
-# Date
+# Louis
+# 11/15/2017
 # Python Beginnings
 # 7 Favorite Color
+import time
 
+password = "tree"
+
+login =  "bob_billy_baconson"
+
+print ("Guess bobs password and login name and be able to hack his computer")
+
+name = input ("name")
+
+print(name)
+
+
+_password_ = input ("What is the password")
+
+if name == login:
+    print ("good job")
+    if password == _password_:
+        print ("oh no you know the password and username I was hoping to blow you up")
+
+    else:
+        print ("Kim's missile launch")
+        print ("3")
+        time. sleep (0.5)
+        print ("1")
+        time. sleep (0.5)
+        print ("Oh wait no 2")
+        time. sleep (0.5)
+        print ("1")
+        time. sleep (0.5)
+        print ("BOOMBOOMBOOMBOOMBOOMBOOM")
+else:
+        print ("Kim's missile launch")
+        print ("3")
+        time. sleep (0.5)
+        print ("1")
+        time. sleep (0.5)
+        print ("Oh wait no 2")
+        time. sleep (0.5)
+        print ("1")
+        time. sleep (0.5)
+        print ("BOOMBOOMBOOMBOOMBOOMBOOM")
---
+++ password_checker.py
@@ -0,0 +1,15 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Admin\Documents\louis\python-control-LouisPow11\c1_7_favorite_color.py 
+Guess bobs password and login name and be able to hack his computer
+nameBob
+Bob
+What is the passwordqwert
+Kim's missile launch
+3
+1
+Oh wait no 2
+1
+BOOMBOOMBOOMBOOMBOOMBOOM
+>>> 
