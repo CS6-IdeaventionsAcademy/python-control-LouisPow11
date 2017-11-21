4e6ebd082920cdb8b1f442bab5278ce29e070c2b
highsociety2 <amcknight@ideaventionsacademy.org>
2017-11-21 16:26:18 -0500

    Color_guessing_game
    hi

--- c1_6_password_checker.py
+++ c1_6_password_checker.py
@@ -1,5 +1,20 @@
-# Name
-# Date
+#Louis
+#2017-11-21
 # Python Beginnings
-# 6 Password Checker
+# 6 Favorite color
+
+
+
+my_favorite_color = "Mikado"
+
+user_color = input ("Guess my color")
+
+while user_color != my_favorite_color:
+
+    print ("Oh my gosh if that was my favorite color then I would not be here, EVER")
+
+    user_color = input ("Guess my color")
+
+print ("good job my next favorite color will be harder")
+
 
